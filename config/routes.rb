168783f4a1bcd91dd89d1ec6a6ca6638ad16127f
shadrach akaade <akaadesm@gmail.com>
2022-05-25 04:00:20 +0100

    create profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :items, except: [:show, :edit, :update] do
+    get :uncategorized, on: :collection
+  end
+  resources :groups
+  devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
